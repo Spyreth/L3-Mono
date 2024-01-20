68a401b3c7c1fa79cea8867030a4aa38d161ec3c
Spyreth <benjamin.pasticier2@gmail.com>
2024-01-20 13:52:31 +0100

    updates on GOL

--- Stage/GOL.py
+++ Stage/GOL.py
@@ -20,8 +20,8 @@ Implemented :
 - Possibility to draw alive and dead cells
 
 To do :
-- Speed control
 - Color control
+- Check for MOUSE_BUTTON_UP to make the drawing easier
 - Grid control
 - Able/disable the periodicity of the grid
 - Make the program run faster or slower without modifying the ability to draw cells fast
@@ -30,6 +30,7 @@ To do :
 colorB = (10,10,10)  # dead cells color (black)
 colorW = (255,255,255)  # live cells color (white)
 colorGrid = (40,40,40)  # grid color FOR IMPLEMENTATION
+
 
 
 def update_cells(cells):
@@ -85,6 +86,42 @@ def update_screen(screen, updated_cells, size):
 
     return 0
 
+
+
+def update(screen,cells,size):
+    """Updates the screen from its previous state. Combination of the previous 2 funcs to only loop one time
+    Args:
+        cells (array): 2D numpy array containing the state of each cell at t0
+        screen (pygame.screen): The screen object from pygame
+        size (int): size in pixels of one cell
+    Returns:
+        cells_updated (array): 2D numpy array containing the state of each cell at t1
+    """
+
+    cells_updated = np.zeros((cells.shape[0],cells.shape[1]))  #Initialization with only zeros of the new array
+
+    #Loop over every cell in the array
+    for i in range(cells.shape[0]):
+        for j in range(cells.shape[1]):
+
+            neighbours = np.sum(cells[i-1:i+2 , j-1:j+2]) - cells[i,j]  #Calcul of the number of neighbors
+            color = colorB
+            
+            if cells[i,j] == 1 :
+                if 2 <= neighbours <= 3:    #if alive and 2/3 neighbours -> alive
+                    cells_updated[i,j] = 1
+                    color = colorW
+            else :
+                if neighbours == 3:   #if dead and 3 neighbours -> alive
+                    cells_updated[i,j] = 1
+                    color = colorW
+            #for every other case the cell is/becomes dead, we keep the value 0 and color black
+            
+            pygame.draw.rect(screen, color, (i*size,j*size,size,size)) #pygame drawing of the cell
+                
+    return cells_updated
+    
+    
     
 
 def main(cell_width, dim1, dim2, fps_max):
@@ -110,10 +147,11 @@ def main(cell_width, dim1, dim2, fps_max):
     screen = pygame.display.set_mode((screendim1,screendim2))
 
     cells0 = np.zeros((dim1,dim2))
-    cells2 = (np.random.rand(dim1,dim2)<0.4).astype(int)
+    
+    #cells2 = (np.random.rand(dim1,dim2)<0.4).astype(int)
     cells = update_cells(cells0)
-
-    screen.fill(colorGrid)
+    
+    screen.fill(colorB)
     pygame.display.flip()
 
     running = False
@@ -126,20 +164,21 @@ def main(cell_width, dim1, dim2, fps_max):
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     running = not running
-                    cells = update_cells(cells)
-                    update_screen(screen,cells,cell_width)
+                    cells = update(screen,cells,cell_width)
                     pygame.display.flip()
             if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
-                cells[pos[0]//cell_width, pos[1]//cell_width] = 1
+                if cells[pos[0]//cell_width, pos[1]//cell_width] == 1:
+                    cells[pos[0]//cell_width, pos[1]//cell_width] = 0
+                else :  
+                    cells[pos[0]//cell_width, pos[1]//cell_width] = 1
                 update_screen(screen,cells,cell_width)
                 pygame.display.update()
 
         # Dessiner vos cellules ici (remplacez cette partie avec votre propre logique)
         screen.fill(colorGrid)
         if running:
-            cells = update_cells(cells)
-            update_screen(screen,cells,cell_width)
+            cells = update(screen,cells,cell_width)
             pygame.display.flip()
         time.sleep(0.001)
 
@@ -156,10 +195,3 @@ if __name__ == '__main__' :
     fps = int(args[3])
 
     main(cell_w, sys_dim1, sys_dim2, fps)
-
-
-
-
-
-
-
