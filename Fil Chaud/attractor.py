def6b299fa4321e32c0993ab003c16ea0114fab0
Spyreth <benjamin.pasticier2@gmail.com>
2024-04-04 00:02:34 +0200

    aaa

--- Fil Chaud/attractor.py
+++ Fil Chaud/attractor.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 def scientific_to_decimal(scientific_str):
     return float(scientific_str)
 
-df = pd.read_csv(r'C:\GIT REPOS\L3-Mono\Fil Chaud\deuxiememesure.csv', converters={'CH1': scientific_to_decimal})
+df = pd.read_csv(r'D:\GIT REPOS\L3 Mono\L3-Mono\Fil Chaud\deuxiememesure.csv', converters={'CH1': scientific_to_decimal})
 
 t = df['X']
 U = df['CH1']
@@ -14,8 +14,9 @@ dt = 10
 U1 = U[:-dt]
 U2 = U[dt:]
 
+colors = np.linspace(0, 1, len(U1))
 plt.figure(figsize=(15,10))
-plt.plot(U1, U2)
+plt.scatter(U1, U2, c=colors, cmap='viridis')
 plt.xlabel('U1')
 plt.ylabel('U2')
 plt.title('Attractor')
